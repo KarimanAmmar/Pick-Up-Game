913fe689b8d96f999a09944921fae77dd8f3f042
DESKTOP-FUD76O6\Kariman <kariman.mohmed@gmail.com>
2023-01-28 03:07:11 +0200

    First Version

---
+++ PickUpGame/Assets/GameSystem/CollectSyatem/ColManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSystem.EndSystem;
+
+namespace GameSystem
+{
+    namespace ColSystem
+    {
+
+        public class ColManager : MonoBehaviour
+        {
+            // Start is called before the first frame update
+
+            [SerializeField] GameObject fakeitem;
+            [SerializeField] EndGame end;
+            [SerializeField] AudioSource sound;
+
+
+            // Update is called once per frame
+            void Update()
+            {
+                
+            }
+            void OnMouseDown()
+            {
+                // Debug.Log(this.name);
+                Destroy(this.gameObject);
+
+                Color tmp = fakeitem.GetComponent<SpriteRenderer>().color;
+                tmp.a = 255f;
+                fakeitem.GetComponent<SpriteRenderer>().color = tmp;
+                end.Count++;
+                sound.Play();
+                
+            }
+
+
+
+        }
+
+    }
+
+}
---
+++ PickUpGame/Assets/GameSystem/EndSystem/EndGame.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+namespace GameSystem
+{
+    namespace EndSystem
+    {
+
+        public class EndGame : MonoBehaviour
+        {
+            // Start is called before the first frame update
+           
+            [SerializeField] GameObject panel;
+           
+            [SerializeField] AudioSource aud;
+            [SerializeField] AudioClip clip;
+
+            public int Count;
+
+
+
+            void Start()
+            {
+               
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                EndGameScene();
+            }
+            void EndGameScene()
+            {
+
+                if (Count == 13)
+                {
+                    panel.SetActive(true);
+                    ReLoad();
+                    aud.PlayOneShot(clip);
+                    Count = 0;
+                }
+                
+            }
+
+            public void ReLoad()
+            {
+                StartCoroutine(IsWaiting());  
+            }
+
+
+            IEnumerator IsWaiting()
+            {
+                yield return new WaitForSeconds(2);
+                SceneManager.LoadScene("UI");
+            }
+            
+        }
+
+    }
+}
